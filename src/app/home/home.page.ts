f9c1f81772a3d149a31b21d1e3bc7d101682ca73
atallah-salah <atallah_salah@outlook.com>
2019-08-31 03:36:19 +0300

    add theme color and more style

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  articles;
+  constructor(private apiService: ApiService) {
+    
+  }
 
-  constructor() {}
 
+  ionViewDidEnter(){
+    this.apiService.getNews().subscribe((data)=>{
+      console.log(data);
+      this.articles = data['articles'];
+    });
+  }
 }
